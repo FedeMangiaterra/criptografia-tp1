b409ef106f63bd554554f2202ba8604ad4de11b6
Fede <ffeede.m@hotmail.com>
2023-04-26 18:26:52 -0300

    Suma

---
+++ finite_field_element.py
@@ -0,0 +1,12 @@
+class FiniteFieldElement:
+    def __init__(self, number, prime):
+        if number >= prime or number < 0:
+            raise ValueError(f"Number has to be between 0 and {prime}")
+        self.number = number
+        self.prime = prime
+
+    def _add_(self, other):
+        if self.prime != other.prime: 
+            raise TypeError('Cannot add two numbers in different Fields')
+        new_number = (self.number + other.number) % self.prime 
+        return self.__class__(new_number, self.prime)
