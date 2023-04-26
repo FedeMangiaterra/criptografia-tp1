3e16fa8a16efd6c1399c62d3a59f1a1e970cd496
Fede <ffeede.m@hotmail.com>
2023-04-26 18:34:37 -0300

    Firmas del resto de metodos

--- finite_field_element.py
+++ finite_field_element.py
@@ -10,3 +10,15 @@ class FiniteFieldElement:
             raise TypeError('Cannot add two numbers in different Fields')
         new_number = (self.number + other.number) % self.prime 
         return self.__class__(new_number, self.prime)
+
+    def _sub_(self, other):
+
+        return
+
+    def _mul_(self, other):
+
+        return
+    
+    def _truediv_(self, other):
+
+        return
