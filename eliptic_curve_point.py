7a312b89e88cea3d13c6d696d60d40ca3159bada
Fede <ffeede.m@hotmail.com>
2023-05-03 01:30:51 -0300

    Empiezo suma de puntos

--- eliptic_curve_point.py
+++ eliptic_curve_point.py
@@ -4,6 +4,8 @@ class ElipticCurvePoint:
         self.b = b
         self.x = x
         self.y = y
+        if self.x is None and self.y is None: 
+            return
         if self.y**2 != self.x**3 + a * x + b: 
             raise ValueError(f'({x}, {y}) is not on the curve')
 
@@ -11,7 +13,28 @@ class ElipticCurvePoint:
         return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
 
     def __add__(self, other):
-        return
+        if self.a != other.a or self.b != other.b:
+            raise TypeError(f'Points {self}, {other} are not on the same curve')
+        # Elemento neutro
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+        
+        #Inverso aditivo
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+        
+        #Mismo punto
+        if self.x == other.x and self.y == other.y:
+            return
+        
+        #Suma de puntos con distinto x, distinto y
+        slope = (other.y - self.y) / (other.x - self.x)
+        new_x = slope ** 2 - self.x - other.x
+        new_y = slope * (self.x - new_x) - self.y
+        return self.__class__(new_x, new_y, self.a, self.b)
+
     
     def duplicate(self):
         return
